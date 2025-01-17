c5d55aaab70af7fa7b8b24b0454950df39e0efdc
carsonmiller31 <carsonmiller31@gmail.com>
2025-01-16 20:41:52 -0700

    Update

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -42,9 +42,4 @@ class Program
     }
 
 
-// DisplayWelcome - Displays the message, "Welcome to the Program!"
-// PromptUserName - Asks for and returns the user's name (as a string)
-// PromptUserNumber - Asks for and returns the user's favorite number (as an integer)
-// SquareNumber - Accepts an integer as a parameter and returns that number squared (as an integer)
-// DisplayResult - Accepts the user's name and the squared number and displays them.   
     }
